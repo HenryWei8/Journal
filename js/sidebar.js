c4f1f5fdd05c4961a8758525b151b4894630c43f
HenryWei8 <143283153+HenryWei8@users.noreply.github.com>
2024-03-02 16:25:37 -0800

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,6 @@
 var sidebarHTML = `
 <div id="mySidebar" class="sidebar" onmouseover="toggleSidebar()" onmouseout="toggleSidebar()">
-<a href="index.html"><span><i class="material-icons">Home</i><span class="icon-text"></span>&nbsp;&nbsp;&nbsp;&nbsp;home</a><br>
+<a href="index.html"><span><i class="material-icons">home</i><span class="icon-text"></span>&nbsp;&nbsp;&nbsp;&nbsp;Home</a><br>
 <a href="https://docs.google.com/document/d/1rlpN8s7TBbchMQgKTx7ZSXBJJbRmq3w1UGj9ncqOm9c/edit?usp=sharing"><i class="material-icons">spa</i><span class="icon-text"></span>&nbsp;&nbsp;&nbsp;&nbsp;Guidelines</span></a><br>
 <a href="papers/consenttopublish.pdf"><i class="material-icons">description</i><span class="icon-text"></span>&nbsp;&nbsp;&nbsp;&nbsp;Agreement Form</span></a><br>
 <a href="mailto: fiatluxjournal@gmail.com"><i class="material-icons">email</i><span class="icon-text"></span>&nbsp;&nbsp;&nbsp;&nbsp;Contact<span></a>
